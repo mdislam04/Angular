c3c00d41d3c634ebdc00d54f589f5a38ffb30dbc
mohmmad.islam <mohmmad.islam@outlook.com>
2018-10-25 15:58:27 +0530

    added dynamic coin add

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data-service.service';
-import { CoinMarket } from './coinmarket';
+import { CoinMarket, Coin } from './coinmarket';
 import { element } from 'protractor';
 
 @Component({
@@ -12,14 +12,17 @@ export class ProfileComponent implements OnInit {
 
   public coinData: CoinMarket;
   public coinDataCopy: CoinMarket;
+  viewData: Coin[] = [];
+  coinToDisplay = ["XRP", "ZRX", "OMG", "GNT"];
   inetrvalId: any;
-  refreshInterval:number = 5;
+  refreshInterval: number = 5;
+  RequestedCoin: string;
   constructor(private service: DataService) { }
 
   ngOnInit() {
     this.getCoinMarketData();
 
-    this.inetrvalId = setInterval(() => this.getCoinMarketData(), this.refreshInterval*1000);
+    this.inetrvalId = setInterval(() => this.getCoinMarketData(), this.refreshInterval * 1000);
 
 
   }
@@ -30,40 +33,50 @@ export class ProfileComponent implements OnInit {
     }
   }
 
-  public restart()
-  {
-    console.log('Restart -- '+this.inetrvalId);
+  public restart() {
     if (this.inetrvalId) {
       clearInterval(this.inetrvalId);
     }
 
-    this.inetrvalId = setInterval(() => this.getCoinMarketData(), this.refreshInterval*1000);
+    this.inetrvalId = setInterval(() => this.getCoinMarketData(), this.refreshInterval * 1000);
+  }
+
+  addCoin() {
+    if (!this.coinToDisplay.find(o => o === this.RequestedCoin.toUpperCase()))
+      this.coinToDisplay.push(this.RequestedCoin.toUpperCase());
   }
 
   public getCoinMarketData() {
     this.service.getCoinMarketData().subscribe(
       data => {
-        this.coinDataCopy = data;
-        if (this.coinDataCopy) {
-          var current = Object.values(this.coinDataCopy.data);
-          //var prev = Object.values(this.prevCoinData.data);
+        this.coinData = data;
+        var current = Object.values(this.coinData.data);
 
-          current.forEach(cur => {
-            if (document.getElementById(cur.name)) {
-              var price = parseFloat(document.getElementById(cur.name).innerText);
-              
-              if (price >= parseFloat(cur.quotes.USD.price.toFixed(5))) {
-                document.getElementById(cur.name).className='high';
-              }
-              else {
-                document.getElementById(cur.name).className='low';
-                console.log(document.getElementById(cur.name).innerText + 'cur price '+cur.quotes.USD.price);
-              }
+        current.forEach(cur => {
+          if (document.getElementById(cur.name)) {
+            var price = parseFloat(document.getElementById(cur.name).innerText);
+
+            if (price >= parseFloat(cur.quotes.USD.price.toFixed(5))) {
+              document.getElementById(cur.name).className = 'high';
             }
+            else {
+              document.getElementById(cur.name).className = 'low';
+              console.log(document.getElementById(cur.name).innerText + 'cur price ' + cur.quotes.USD.price);
+            }
+          }
 
-          });
-        }
-        this.coinData = this.coinDataCopy;
+        });
+
+        //Set Coin to display
+        this.viewData = [];
+        this.coinToDisplay.sort().forEach(coin => {
+          var match = current.find(o => o.symbol == coin);
+          this.viewData.push(match);
+        });
+
+
+
+
       }
     );
   }
