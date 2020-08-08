78bc88bb05fd08e388c41b7914ee198d24f9f11d
Islam, Mohmmad <mohmmad.islam@accenture.com>
2020-08-08 11:12:54 +0530

    updated menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,4 +41,15 @@ export class AppComponent {
     });
   }
 
+   openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+   closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
 }
