d67a3fa48264b7a1cc84e31ca992a9eac53824db
mdislam04 <mdislam04@users.noreply.github.com>
2020-12-31 18:19:38 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  token : 'sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-12-31T00:30:39Z&st=2020-03-31T16:30:39Z&spr=https&sig=5JvB34r9rlNALJlnIicllhUXKXF0cT5XoUiBJeDaAyk%3D'
+  token : 'sv=2019-12-12&ss=bfqt&srt=co&sp=rwdlacupx&se=2021-12-30T20:41:46Z&st=2020-12-31T12:41:46Z&spr=https&sig=9BQuAOsSdJDC%2Bz5w5khp6jeJseeG5Lb3pzR%2B5r3JAtg%3D'
 };
